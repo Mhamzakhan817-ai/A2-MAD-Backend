304ea20ef7aa1502d88a22c8fbbc202a79538b23
Muhammed Hamza Khan <hamzamuhammed817@gmail.com>
2025-11-30 20:52:23 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 require('dotenv').config();
 
-// Connect to MongoDB
 connectDB();
 
 const app = express();
@@ -11,24 +10,16 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 
-// Default route
 app.get('/', (req, res) => {
   res.send('Top Fabrics Retail Backend is running...');
 });
 
-// EXPORT app for Vercel
-module.exports = app;
-
-// LOCAL development ONLY
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 8000;
-  app.listen(PORT, () =>
-    console.log(`Server running locally on port ${PORT}`)
-  );
-}
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
