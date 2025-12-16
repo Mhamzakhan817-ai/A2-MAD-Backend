14c83a012afc19bdb2ccdf33665a85646e654a1e
Muhammed Hamza Khan <hamzamuhammed817@gmail.com>
2025-12-16 22:19:38 +0500

    Change JSON limit and update string quotes
    Updated JSON parsing limits and fixed string quotes.

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 connectDB();
 
 const app = express();
+
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/cart', require('./routes/cartRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
-app.get('/', (req, res) => {
-  res.send('Top Fabrics Retail Backend is running...');
+app.get("/", (req, res) => {
+  res.send("Top Fabrics Retail Backend is running...");
 });
 
 const PORT = process.env.PORT || 8000;
