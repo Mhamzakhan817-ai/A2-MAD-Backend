cc1c78161ad98d135742a9dcbff4a3a7a2c570c8
Muhammed Hamza Khan <hamzamuhammed817@gmail.com>
2025-12-16 22:17:28 +0500

    Fix route parameter naming for getOrders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-
 router.post('/create', orderController.createOrder);
-router.get('/:user_id', orderController.getOrders);
-
+router.get('/:userId', orderController.getOrders);
 
 module.exports = router;
