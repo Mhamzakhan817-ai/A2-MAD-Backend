077b2996e9af31a48d6152280d36512446d1f4f7
Muhammed Hamza Khan <hamzamuhammed817@gmail.com>
2025-12-16 22:17:51 +0500

    Add wishlist route to product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-
 router.get('/', productController.getAllProducts);
 router.get('/featured', productController.getFeatured);
+router.get('/wishlist', productController.getWishlistProducts); // GET /wishlist?ids=...
 router.get('/:id', productController.getProductById);
-
 
 module.exports = router;
